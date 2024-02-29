66449d45d5043635f3b1e5ea4f850ca77fa09dd1
Markizano Draconus <markizano@markizano.net>
2024-02-28 20:33:49 -0600

    sign and checksum stuff after running sdist.

--- setup.py
+++ setup.py
@@ -70,3 +70,30 @@ if 'DEBUG' in os.environ: pprint(setup_opts)
 
 setup(**setup_opts)
 
+if 'sdist' in sys.argv:
+    import gnupg, hashlib
+    gpg = gnupg.GPG()
+    for artifact in glob('dist/*.tar.gz'):
+        # Detach sign the artifact in dist/ folder.
+        fd = open(artifact, 'rb')
+        checksums = open('dist/CHECKSUMS.txt', 'w+b')
+        status = gpg.sign_file(fd, detach=True, output=f'{artifact}.asc')
+        print(f'Signed {artifact} with {status.fingerprint}')
+
+        # create a MD5, SHA1 and SHA256 hash of the artifact.
+        for hashname in ['md5', 'sha1', 'sha256']:
+            hasher = getattr(hashlib, hashname)()
+            fd.seek(0,0)
+            hasher.update(fd.read())
+            digest = hasher.hexdigest()
+            checksums.write(f'''{hashname.upper()}:
+{digest} {artifact}
+
+'''.encode('utf-8'))
+            print(f'Got {artifact}.{hashname} as {digest}')
+        checksums.seek(0, 0)
+        chk_status = gpg.sign_file(checksums, detach=True, output=f'dist/CHECKSUMS.txt.asc')
+        checksums.close()
+        fd.close()
+        print(f'Signed CHECKSUMS.txt with {chk_status.fingerprint}')
+
