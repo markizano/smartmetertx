b95f97d8d7e2c1e14540bde0c3e0efd18ec50d64
Markizano Draconus <markizano@markizano.net>
2026-04-04 23:32:46 -0500

    including proper imports.

--- lib/smartmetertx/__init__.py
+++ lib/smartmetertx/__init__.py
@@ -1,4 +1,12 @@
 from kizano import Config
-Config.APP_NAME = 'smartmetertx'
-from smartmetertx.api import MeterReader
-from smartmetertx.smtx2mongo import Smtx2Mongo
+Config.setAppName('smartmetertx')
+
+import smartmetertx.api as api
+import smartmetertx.controller as controller
+import smartmetertx.notify as notify
+import smartmetertx.meterReport as meterReport
+import smartmetertx.server as server
+import smartmetertx.smtx2mongo as smtx2mongo
+import smartmetertx.utils as utils
+
+__all__ = ['api', 'controller', 'notify', 'meterReport', 'server', 'smtx2mongo', 'utils']
